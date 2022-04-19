95dbd8fa54dc85b49cbacc34cb7166d91706f42a
arcodez <abelileitor123@gmail.com>
2022-04-18 23:53:29 -0400

    update

--- DolarCheckerbot/bot.js
+++ DolarCheckerbot/bot.js
@@ -1,10 +1,45 @@
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf("5243868127:AAE6_-8q4Yzl-pRN4nxDCgPRDPk11TZcWIE");
 
-bot.start((ctx) => ctx.reply("Welcome!"));
+// Seriales de los billetes escoñetaos
+const billetes = [
+  {
+    type: "1",
+    serial: ["122D", "FG1F"],
+  },
+  {
+    type: "5",
+    serial: ["321D"],
+  },
+  {
+    type: "20",
+    serial: ["8789"],
+  },
+];
 
-bot.help((ctx) => ctx.reply("Send me a sticker"));
+// create a array of billetes serials and unnested the array
+const billetesSerial = billetes.reduce((acc, cur) => {
+  return acc.concat(cur.serial);
+}, []);
 
-bot.on("sticker", (ctx) => ctx.reply("👍"));
+console.log(billetesSerial);
+
+const serialsMoney = ["122D", "AAA2"];
+
+// Inicio del bot
+bot.start((ctx) => ctx.reply("Enviame los 4 ultimos digitos del billete"));
+
+// add function to validate serialsMoney
+bot.on("text", (ctx) => {
+  const serial = ctx.message.text;
+  if (billetesSerial.includes(serial)) {
+    ctx.reply("Si te acepto este billete");
+  } else {
+    ctx.reply("Vete con tu billete mamalon");
+  }
+});
+
+// esto casi lo mismo de arriba pero no lo manda a mamar
+bot.hears(serialsMoney, (ctx) => ctx.reply("Es valido gracias por comprar"));
 
 bot.launch();
